59f91bd62d4f886619f306a8c13ce600a9cb2e9a
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-07-12 17:15:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,12 +6,11 @@ function setup()
 {
     canvas=createCanvas(400,400);
     canvas.center();
-    video=createCapture(VIDEO)
+    video=createCapture(VIDEO);
     video.hide();
 
     poseNet= ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
-
 }
 
 function modelLoaded()
@@ -19,23 +18,33 @@ function modelLoaded()
     console.log(" The poseNet model is loaded")
 }
 
+
 function draw()
 {
-    image(video,0,0, 400,400)
+image(video,0,0, 400,400);
+circle(Nose_x, Nose_y, 20);
+fill(225,0,0);
+stroke(225,0,0);
 }
+Nose_x=0;
+Nose_y=0;
 
 function gotPoses(results)
 {
 if(results.length>0)
 {
     console.log(results)
-    console.log("The lips x=" + results[0].pose.lips.x);
-    console.log("The lips y=" + results[0].pose.lips.y);
-
+    Nose_x=results[0].pose.nose.x;
+    Nose_y=results[0].pose.nose.y;
+    console.log("The nose x=" + Nose_x);
+    console.log("The nose y=" + Nose_y);
+;
 
 }
 }
+
+
 function take_snapshot()
 {
-    save("lipstick_filter.png")
+    save("Clown_filter.png")
 }
