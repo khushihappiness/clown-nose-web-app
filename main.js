fba059b8351feda0637488d70b209284ae25116e
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-07-12 17:13:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ function setup()
 {
     canvas=createCanvas(400,400);
     canvas.center();
-    video=createCapture(VIDEO);
+    video=createCapture(VIDEO)
     video.hide();
 
     poseNet= ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
+
 }
 
 function modelLoaded()
@@ -18,10 +19,9 @@ function modelLoaded()
     console.log(" The poseNet model is loaded")
 }
 
-
 function draw()
 {
-image(video,0,0, 400,400)
+    image(video,0,0, 400,400)
 }
 
 function gotPoses(results)
@@ -29,15 +29,13 @@ function gotPoses(results)
 if(results.length>0)
 {
     console.log(results)
-    console.log("The nose x=" + results[0].pose.nose.x);
-    console.log("The nose y=" + results[0].pose.nose.y);
+    console.log("The lips x=" + results[0].pose.lips.x);
+    console.log("The lips y=" + results[0].pose.lips.y);
 
 
 }
 }
-
-
 function take_snapshot()
 {
-    save("Clown_filter.png")
+    save("lipstick_filter.png")
 }
